04308690a28a6661cd2f843953bc57da91a11970
TMShumway <ryujindra@gmail.com>
2021-04-27 17:44:06 -0600

    complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Timothy Shumway";
+    int age = 31;
+    String state = "Texas";
+
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+
+  }
+}
