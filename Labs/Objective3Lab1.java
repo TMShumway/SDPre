18b1bdfd4787a54dac1b4134114b3b6b1cc60376
TMShumway <ryujindra@gmail.com>
2021-04-27 18:23:50 -0600

    complete Objective3Lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,14 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+
+    num1 = 12.0;
+    num2 = 7.75;
+    num3 = 8.90;
+
+    average = (num1 + num2 + num3)/3;
+
+    System.out.println(average);
+
+  }
+}
