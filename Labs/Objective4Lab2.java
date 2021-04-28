a631ca3f83e3806800610f0802840135610294e9
TMShumway <ryujindra@gmail.com>
2021-04-27 21:54:26 -0600

    complete Objective4Lab2

--- Labs/Objective4Lab2.java
+++ Labs/Objective4Lab2.java
@@ -27,10 +27,5 @@ public class Objective4Lab2 {
 
     System.out.println("The sum of " + dub1 + " + " + dub2 +
                        " + " + dub3 + " = " + (dub1 + dub2 + dub3));
-
-    //TODO print the three ints and their sum.
-
-    //TODO print the three doubles and their sum.
-
   }
 }
