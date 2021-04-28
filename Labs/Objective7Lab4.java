90fc7c86aea0301f488a3d9517a79b14d499e3ef
TMShumway <ryujindra@gmail.com>
2021-04-28 17:53:33 -0600

    complete Objective7Lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,14 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+    int sum = 0;
+    int n = 1;
+
+    while(n <= 20) {
+      sum += n;
+      n++;
+    }
+
+    System.out.println(sum);
+
+  }
+}
