45620c557ea3acfc1afb513b11662d1d339032a0
TMShumway <ryujindra@gmail.com>
2021-04-28 21:19:05 -0600

    complete Objective8Lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,11 @@
+public class Objective8Lab2 {
+  public static void main(String[] args) {
+    int runningSum = 0;
+
+    for(int i = 1; i <= 20; i++) {
+      runningSum += i;
+    }
+
+    System.out.println(runningSum);
+  }
+}
